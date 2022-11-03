23f8e59b6e5730ae57e172b86263840aa55c25fa
johnchem <j.payssan@gmail.com>
2022-11-03 18:53:15 +0100

    login page css adjustment

--- litreview/authentication/forms.py
+++ litreview/authentication/forms.py
@@ -10,14 +10,18 @@ from django.contrib.auth import get_user_model
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length=63)
-    password = forms.CharField(max_length=63, widget=forms.PasswordInput)
+    username = forms.CharField(
+        label='',
+        max_length=63)
+    password = forms.CharField(
+        max_length=63, 
+        widget=forms.PasswordInput)
     
     def __init__(self, *args, **kargs):
         super().__init__(*args, **kargs)
 
         for field in self.fields:
-            self.fields[field].help_text = None
+            # self.fields[field].help_text = None
             self.fields[field].label = ''
         
         self.fields["username"].widget.attrs.update({
@@ -33,11 +37,15 @@ class LoginForm(forms.Form):
         helper = FormHelper()
         helper.form_id = 'login__form'
         helper.form_method = 'POST'
+        helper.form_show_errors = True
+        helper.error_text_inline = True
         # self.helper.add_input(Submit("login__button","Se Connecter"))
         helper.add_input(Submit(name="login__button",
                     value="Se Connecter",
                     css_class="login__button button"))
         return helper
+    
+
 
 
 class SignUpForm(UserCreationForm):
