9ec4c04bad722d578faf7303e62479bd6c39764d
johnchem <j.payssan@gmail.com>
2022-11-10 11:56:49 +0100

    debug sub people and followed user & error message

--- litreview/network/views.py
+++ litreview/network/views.py
@@ -18,11 +18,11 @@ class UserNetwork(LoginRequiredMixin, View):
     def get(self, request, *args, **kwargs):
         searchbox_form = self.form_class_search_box()
         
+        # display users followed by the login user
         user = request.user
         followed_users = [x.followed_user for x in models.UserFollows.objects.filter(user=user)]
-        print(*followed_users)
         
-
+        # display users who follow the login user
         followers = [x.user.username for x in models.UserFollows.objects.filter(followed_user=user)]
 
         return render(
@@ -37,24 +37,46 @@ class UserNetwork(LoginRequiredMixin, View):
         )
 
     def post(self, request, *args, **kwargs):
-        if request.POST["search__button"]:
-            
-            followed_user = User.objects.filter(username=request.POST["username"])[0]
-            item = models.UserFollows(user=request.user, followed_user=followed_user)
-            item.save()
+        # display the search box form              
         searchbox_form = self.form_class_search_box()
         
+        # display users followed by the login user
         user = request.user
         followed_users = [x.followed_user for x in models.UserFollows.objects.filter(user=user)]
-        print(*followed_users)
         
+        # display users who follow the login user
         followers = [x.user.username for x in models.UserFollows.objects.filter(followed_user=user)]
 
+        # user clicked on the search__button
+        search_messages = []
+        if request.POST["search__button"]:
+            user_input = request.POST["username"]
+            try:
+                input_validation = True
+                follow_user = User.objects.get(username=user_input)
+                if follow_user == request.user:
+                    input_validation = False
+                    search_messages.append("Vous ne pouvez pas vous suivre vous-même")
+                if follow_user in followed_users:
+                    input_validation = False
+                    search_messages.append(f'Vous suivez déjà {user_input}')
+
+                if input_validation:
+                    item = models.UserFollows(user=request.user, followed_user=follow_user)
+                    item.save()
+                    # update list of followed people
+                    followed_users = [x.followed_user for x in models.UserFollows.objects.filter(user=user)]
+            
+            except BaseException as err:
+                print(f'DEBUG : {err}') 
+                search_messages.append(f"l'utilisateur {user_input} n'existe pas")
+        
         return render(
             request,
             self.template_name,
             context={
                 "name":"Abonnement",
+                "search_messages": search_messages,
                 "searchbox_form": searchbox_form,
                 "followed_users": followed_users,
                 "followers": followers,
